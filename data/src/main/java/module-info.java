027c60b2891a2c38e881861a9f242304025edc79
jamhour1g <75880084+jamhour1g@users.noreply.github.com>
2025-08-24 01:33:14 +0300

    Refactor `module-info.java` and relocate `JpaManager`
    - Moved `JpaManager` class from `com.azkar.persistence` to `com.azkar.config`
    to better reflect its role in configuration management.
    
    - Adjusted dependencies in `module-info.java`:
      - Removed unused `jakarta.inject` and `jakarta.cdi`.
      - Moved `lombok` and `org.jspecify` to `requires static` for compile-time use.
      - Added `com.azkar.config` to `exports` for external accessibility.

--- data/src/main/java/module-info.java
+++ data/src/main/java/module-info.java
@@ -3,14 +3,14 @@ module com.azkar.data {
     requires java.sql;
     requires org.xerial.sqlitejdbc;
     requires jakarta.persistence;
-    requires org.jspecify;
     requires org.hibernate.orm.core;
     requires jakarta.transaction;
-    requires jakarta.inject;
-    requires jakarta.cdi;
-    requires static lombok;
     requires com.azkar.utils;
     requires org.flywaydb.core;
+    
+    // Compile-time only
+    requires static lombok;
+    requires static org.jspecify;
 
     opens com.azkar.data.entity to
             org.hibernate.orm.core;
@@ -18,6 +18,7 @@ module com.azkar.data {
             org.hibernate.orm.core;
 
     exports com.azkar.data.model;
+    exports com.azkar.config;
     exports com.azkar.data.entity;
     exports com.azkar.data.repo;
 }
